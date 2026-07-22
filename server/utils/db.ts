0c1d2b9f8c1c095f3a554503b152cf217b92f5ee
ShortCake03 <ShortCake03@users.noreply.github.com>
2026-07-22 21:11:52 +0200

    Fix db.ts and package dependencies

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,13 +1,12 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3'
-import Database from 'better-sqlite3'
+import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client'
 import * as schema from '../database/schema'
 import path from 'path'
 
 const dbPath = path.resolve(process.cwd(), 'sqlite.db')
-const sqlite = new Database(dbPath)
+const client = createClient({
+  url: `file:${dbPath}`
+})
 
-// Activer les clés étrangères pour SQLite
-sqlite.pragma('foreign_keys = ON')
-
-export const db = drizzle(sqlite, { schema })
+export const db = drizzle(client, { schema })
 export { schema }
